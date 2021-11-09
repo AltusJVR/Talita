d3d78d3b91b850a6cad9e0368cd2e0ba046c1fe5
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-11-09 15:54:03 -0300

    add 9 nov class

--- 26-Oct/app.js
+++ 26-Oct/app.js
@@ -1,38 +1,44 @@
 /* Spreading with arrays */
-
+/* spread = concat */
 const numbers = [1, 2, 3, 4, 5];
 const numbers2 = [...numbers, 6, 7, 8, 9];
 const numbers3 = [...numbers2, "ten"];
-console.log(numbers, "numbers");
-console.log(numbers2, "numbers2");
-console.log(numbers3, "numbers3");
+const object = {
+  value: 1,
+  number: 20,
+};
+const numbers4 = [...numbers3, object];
+// console.log(numbers, "numbers");
+// console.log(numbers2, "numbers2");
+// console.log(numbers3, "numbers3");
+// console.log(numbers4, "numbers4");
 
-/* spreading with Objects */
+// /* spreading with Objects */
 
 const user = {
   name: "Betty",
   married: false,
   age: 25,
   occupation: "Engineer",
-};
-
-const newUser = { ...user, city: "Rio" };
-console.log(newUser, "newUser");
-
-const user2 = {
-  /*   name: "John",
-  married: true,
-  age: 25, */
-  occupation: "Doctor",
-  city: "Amparo",
   userName: "DocJohn",
 };
 
+const newUser = { ...user, city: "Rio" };
+// console.log(newUser, "newUser");
+
+const user2 = {
+  name: "John",
+  married: true,
+  age: 25,
+  occupation: "Doctor",
+  city: "Amparo",
+};
+
 const newUser2 = { ...user, ...user2 };
-console.log(newUser2, "newUser2");
-/* spread with objects and arrays */
+// console.log(newUser2, "newUser2");
+// /* spread with objects and arrays */
 const newUser3 = { ...user, favNums: numbers };
-console.log(newUser3, "newUser3");
+// console.log(newUser3, "newUser3");
 
 const newUser4 = {
   name: "John",
@@ -43,17 +49,20 @@ const newUser4 = {
   userName: "DocJohn",
   favNums: numbers,
 };
-console.log(newUser4, "newUser4");
+
+// console.log(newUser4, "newUser4");
 
 const nums = { ...numbers };
 
-console.log(nums, "nums");
+// console.log(nums, "nums");
 
-const users = [newUser];
-console.log(users, "users");
+const users = [newUser4];
 
-const updateUsers = (user) => {
-  return (updatedUsers = [...users, user]);
+// [{ object }, { object }];
+// console.log(users, "users");
+
+const updateUsers = () => {
+  return (updatedUsers = [...users, newUser4]);
 };
 
-console.log(updateUsers(newUser4), "update users");
+console.log(updateUsers(), "update users");
